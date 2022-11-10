8c3aad6f1116834ca0d552e418f4801f5a4c7537
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-11-10 14:19:57 +0100

    Added Car and Person entity and connected them

---
+++ src/main/java/com/johanekstroem/parking/Entities/Car.java
@@ -0,0 +1,52 @@
+package com.johanekstroem.parking.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Car {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  long id;
+  String registrationNumber;
+  
+  
+  @ManyToOne
+  @JoinColumn(name="person_id")
+  private Person person;
+
+
+  public long getId() {
+    return id;
+  }
+
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getRegistrationNumber() {
+    return registrationNumber;
+  }
+
+
+  public void setRegistrationNumber(String registrationNumber) {
+    this.registrationNumber = registrationNumber;
+  }
+
+
+  public Person getPerson() {
+    return person;
+  }
+
+
+  public void setPerson(Person person) {
+    this.person = person;
+  }
+
+}
---
+++ src/main/java/com/johanekstroem/parking/Entities/Person.java
@@ -0,0 +1,61 @@
+package com.johanekstroem.parking.Entities;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Person {
+  
+  public long getId() {
+    return id;
+  }
+  public void setId(long id) {
+    this.id = id;
+  }
+  public String getFirstName() {
+    return firstName;
+  }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+  public String getLastName() {
+    return lastName;
+  }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+  public Set<Car> getCars() {
+    return cars;
+  }
+  public void setCars(Set<Car> cars) {
+    this.cars = cars;
+  }
+
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  long id;
+  String firstName;
+  String lastName;
+  
+
+  
+  public Person(){}
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+
+@OneToMany(mappedBy ="person")
+    private Set<Car> cars;
+
+  
+
+
+}
