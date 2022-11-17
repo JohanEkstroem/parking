922991253b2c3c43230c019028fc3e24643c2d13
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-11-17 15:22:59 +0100

    Added empty template for Parking Event

---
+++ src/main/java/com/johanekstroem/parking/Entities/ParkingEvent.java
@@ -0,0 +1,21 @@
+package com.johanekstroem.parking.Entities;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ParkingEvent {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  //  What information does a parking event need?
+  //  *Car
+  //  *ParkingSpot
+  //  *Start time
+  //  *Stop time
+  //  *Is parking event stopped or ongoing?
+
+}
