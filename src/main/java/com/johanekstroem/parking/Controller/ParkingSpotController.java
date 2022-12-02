4b45d541b4d53a73b26d7fd344d12b36d617c20f
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-12-02 14:39:09 +0100

    Refactored return type

--- src/main/java/com/johanekstroem/parking/Controller/ParkingSpotController.java
+++ src/main/java/com/johanekstroem/parking/Controller/ParkingSpotController.java
@@ -1,13 +1,16 @@
 package com.johanekstroem.parking.Controller;
 
-import java.util.Optional;
+import java.net.URI;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.johanekstroem.parking.Entities.ParkingSpot;
 import com.johanekstroem.parking.Repositories.ParkingSpotRepository;
 
@@ -30,18 +33,25 @@ public class ParkingSpotController {
   }
 
   @PostMapping("/parkingspot")
-  public ParkingSpot points(@RequestBody ParkingSpot parking) {
-    return parkingSpotRepository.save(parking);
+  public ResponseEntity<ParkingSpot> points(@RequestBody ParkingSpot parking) {
+    var parkingSpot = parkingSpotRepository.save(parking);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .buildAndExpand(parkingSpot.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(parkingSpot);
   }
 
   
   @GetMapping("/parkingspot/{id}")
-  public Optional<ParkingSpot> getAllCustomerByID(@PathVariable("id") Long id) {
+  public ResponseEntity<ParkingSpot> getAllCustomerByID(@PathVariable("id") Long id) {
     var parkingspot = parkingSpotRepository.findById(id);
     if (parkingspot.isPresent()) {
-      return parkingspot;
+      return ResponseEntity.ok().body(parkingspot.get());
     }
-    return null;
+    return ResponseEntity.notFound().build();
   }
 
   @GetMapping("/parkingspot")
