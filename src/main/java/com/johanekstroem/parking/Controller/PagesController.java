7b680314223346964581b47d17614cd95bb24793
clindforsSejsing <c.lindforssejsing@gmail.com>
2022-12-08 13:29:40 +0100

    connected css.file to dev- and backend server. Added controller.

---
+++ src/main/java/com/johanekstroem/parking/Controller/PagesController.java
@@ -0,0 +1,20 @@
+package com.johanekstroem.parking.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController {
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String viewLoginPage(Model model){
+        return "login";
+    }
+
+}
