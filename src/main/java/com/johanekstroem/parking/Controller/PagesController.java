65a752a8980553d8c97ef70da96c08664b02d0a4
clindforsSejsing <c.lindforssejsing@gmail.com>
2022-12-09 14:04:28 +0100

    Added login, css, and tiny bit of implemented bootstrap.

--- src/main/java/com/johanekstroem/parking/Controller/PagesController.java
+++ src/main/java/com/johanekstroem/parking/Controller/PagesController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class PagesController {
 
@@ -15,6 +16,11 @@ public class PagesController {
     @GetMapping("/login")
     public String viewLoginPage(Model model){
         return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
     }
 
 }
