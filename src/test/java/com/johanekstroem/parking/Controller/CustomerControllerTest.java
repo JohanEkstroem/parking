bfa95ca8691f5f9a48a2ba3c7e28bc507fc98bdf
kaulfield23 <kaulfield23@gmail.com>
2022-12-15 10:38:15 +0100

    deleted useless code

--- src/test/java/com/johanekstroem/parking/Controller/CustomerControllerTest.java
+++ src/test/java/com/johanekstroem/parking/Controller/CustomerControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -55,7 +56,6 @@ public class CustomerControllerTest {
 
         Mockito.when(customerRepo.save(Mockito.any(Customer.class))).thenReturn(customer);
 
-        System.out.println(customer);
         mockMvc.perform(post("/api/customer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(customer))
