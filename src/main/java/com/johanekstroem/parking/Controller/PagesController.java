1fe6b267a8bbdb5143f4dc0b6b7344e8dd081cb4
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-12-16 10:58:22 +0100

    Add RestTemplate and attribute

--- src/main/java/com/johanekstroem/parking/Controller/PagesController.java
+++ src/main/java/com/johanekstroem/parking/Controller/PagesController.java
@@ -1,12 +1,29 @@
 package com.johanekstroem.parking.Controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.client.RestTemplate;
+
+import com.johanekstroem.parking.Models.UserCredentials;
 
 @Controller
 public class PagesController {
-
+    @Autowired
+    RestTemplate restTemplate;
     @GetMapping("/")
     public String viewHomePage(Model model){
         return "index";
@@ -19,6 +36,7 @@ public class PagesController {
 
     @GetMapping("/createuser")
     public String viewCreateUserPage(Model model) {
+        model.addAttribute("UserCredentials", new UserCredentials());
         return "createuser";
     }
 
@@ -27,5 +45,6 @@ public class PagesController {
         return "/error/access-denied";
     }
 
+ 
 
 }
