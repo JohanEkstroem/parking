47e9ed0290356eae3c380a1a14eb53f88b07a4a9
kaulfield23 <kaulfield23@gmail.com>
2022-12-16 14:44:40 +0100

    send dummy user data to frontend

--- src/main/java/com/johanekstroem/parking/Controller/PagesController.java
+++ src/main/java/com/johanekstroem/parking/Controller/PagesController.java
@@ -1,5 +1,6 @@
 package com.johanekstroem.parking.Controller;
 
+import com.johanekstroem.parking.Entities.Customer;
 import com.johanekstroem.parking.Entities.ParkingEvent;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,39 +10,50 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
 @Controller
 public class PagesController {
 
     @GetMapping("/")
-    public String viewHomePage(){
+    public String viewHomePage() {
         return "index";
     }
 
- @RequestMapping(value = "/parking", method = RequestMethod.POST)
-    public String createParking(@ModelAttribute ParkingEvent parkingevent){
-       return ResponseEntity.ok().toString();
+    @RequestMapping(value = "/parking", method = RequestMethod.POST)
+    public String createParking(@ModelAttribute ParkingEvent parkingevent) {
+        return ResponseEntity.ok().toString();
 
     }
-    @GetMapping ("/parking")
-    public String viewParkingPage(Model model){
-        model.addAttribute("parkingEvent",new ParkingEvent());
+
+    @GetMapping("/parking")
+    public String viewParkingPage(Model model) {
+        model.addAttribute("parkingEvent", new ParkingEvent());
         return "parkingevent";
     }
 
+    @GetMapping("/car")
+    public String viewCarRegisterPage(Model model) {
+        // I set dummy codes until I can get req body from login page
+        var customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Haeju");
+        customer.setLastName("Dummy");
+
+        model.addAttribute("customer", customer);
+        return "carregister";
+    }
+
     @GetMapping("/login")
-    public String viewLoginPage(){
+    public String viewLoginPage() {
         return "login";
     }
 
     @GetMapping("/saved")
-    public String viewParkingStarted(){
+    public String viewParkingStarted() {
         return "saved";
     }
 
     @GetMapping("/ops")
-    public String viewParkingNotStarted(){
+    public String viewParkingNotStarted() {
         return "ops";
     }
 
@@ -49,6 +61,5 @@ public class PagesController {
     public String accessDenied() {
         return "/error/access-denied";
     }
-
 
 }
