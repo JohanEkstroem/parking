9b04dbb04b2d6b467549e3b9fed0fca94717e089
kaulfield23 <kaulfield23@gmail.com>
2022-12-19 14:42:23 +0100

    take away useless codes

--- src/main/java/com/johanekstroem/parking/Controller/CustomerCarController.java
+++ src/main/java/com/johanekstroem/parking/Controller/CustomerCarController.java
@@ -4,13 +4,11 @@ import java.net.URI;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
