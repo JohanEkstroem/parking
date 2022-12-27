0e35352a6f1b674f70ab7daeb12d3531f53e47a6
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-12-27 20:34:40 +0100

    Add boilerplate ParkingEvent

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import "./App.css";
-import Header from "./components/Header.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Login from "./components/Login.jsx";
-import Car from "./components/Car.jsx";
-import Ops from "./components/Ops.jsx";
-import ProfilePage from "./components/ProfilePage";
-import CreateUser from "./components/CreateUser";
-import { useLocation } from "react-router-dom";
+import './App.css';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
+import Login from './components/Login.jsx';
+import Car from './components/Car.jsx';
+import Ops from './components/Ops.jsx';
+import ProfilePage from './components/ProfilePage';
+import CreateUser from './components/CreateUser';
+import { useLocation } from 'react-router-dom';
 import {
   BrowserRouter,
   Routes,
   Route,
   NavLink,
   Router,
-} from "react-router-dom";
-import Home from "./components/Home";
+} from 'react-router-dom';
+import Home from './components/Home';
+import ParkingEvent from './components/ParkingEvent';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route exact path="/car" element={<Car />} />
           <Route exact path="/ops" element={<Ops />} />
           <Route exact path="/profile" element={<ProfilePage />} />
+          <Route exact path="/parkingevent" element={<ParkingEvent />} />
         </Routes>
       </BrowserRouter>
     </>
